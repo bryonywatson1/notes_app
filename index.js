1ef42fc3d9199fadb80bf9fc7d656dfa34860b6f
George Seeger <georgeseeger@hotmail.com>
2017-01-04 12:12:41 +0000

    Allow creation of new notes

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ window.onload = function(){
   nc.addFavourite();
   nc.showNotes();
 
-  document.getElementById('app').addEventListener("click", function(clickEvent){
+  window.addEventListener("hashchange", function(clickEvent){
     var id = nc.getNoteId();
     nc.displaySingleNote(id);
+  })
+
+  document.getElementById('home').addEventListener('click'), function(){
+    nc.showNotes();
+  }
+
+  window.addEventListener('submit', function(submitEvent){
+    submitEvent.preventDefault();
+    var text = document.getElementById('text').value;
+    nc.newNote(text);
+    nc.showNotes();
+    document.getElementById('text').value = "";
   })
 }
--- note-controller.js
+++ note-controller.js
@@ -5,6 +5,9 @@
 NoteController.prototype.addFavourite = function(){
   this.notelist.storeNote(new Note("Favourite Drink: seltzer"));
 }
+NoteController.prototype.newNote = function(text){
+  this.notelist.storeNote(new Note(text));
+}
 NoteController.prototype.showNotes = function(){
   document.getElementById('app').innerHTML = new NoteListView(this.notelist).view();
 };
